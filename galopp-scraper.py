880c40f722229c811d5bc63d195ee91c27062e1e
Tim Anders <timanders.95@hotmail.de>
2020-11-18 02:21:55 +0100

    Finished scraping process

--- galopp-scraper.py
+++ galopp-scraper.py
@@ -6,12 +6,13 @@ from bs4 import BeautifulSoup
 URL = "https://www.galopp-statistik.de/DisplayErgebnis.php?id="
 
 def scrape_race_by_id(id):
-    id = 9100
+    id = 9100 # DELETE ME
+    race_information = []
 
     # General race information
     date = location = distance = ground_state = prize = None
 
-    print("Scraping page: {id}")
+    print(f"Scraping page: {id}")
     request = requests.get(URL + str(id))
     soup = BeautifulSoup(request.content, "lxml")
 
@@ -21,23 +22,34 @@ def scrape_race_by_id(id):
     location = header.find_all("div", class_="ZeitundOrt")[1]
 
     # Get distance and prize
-    distance = soup.find("div", class_="zoile distance-cash")
-    distance_prize = distance.text.split("-")
+    distance_prize = soup.find("div", class_="zoile distance-cash")
+    distance_prize = distance_prize.text.split("-")
     distance = distance_prize[0]
     prize = distance_prize[1]
 
     # Get category and race class
-    tmp = str(soup.find("div", class_="zoile kat-class"))
-    category_race = re.findall(r"<b>.*</b>", str(tmp))
+    category_race = str(soup.find("div", class_="zoile kat-class"))
+    category_race = re.findall(r"<b>.*</b>", str(category_race))
     category = category_race[0][3:-4]
     race_class = category_race[1][3:-4]
 
     # Get the state of the ground
     ground_state = soup.find("div", class_="rennen-druck").text
 
+    # Put all info into the race information list
+    race_information.append(header)
+    race_information.append(date)
+    race_information.append(location)
+    race_information.append(distance)
+    race_information.append(prize)
+    race_information.append(category)
+    race_information.append(race_class)
+    race_information.append(ground_state)
+
     # Get all horse entries
     entries = soup.find_all("div", class_="table-result-row")
     for entry in entries:
+        horse_entry = []
 
         # (Re)set horse information
         trainer_name = weight  = category = race_class = None
@@ -51,8 +63,18 @@ def scrape_race_by_id(id):
         trainer = jockey_trainer[1].text
         weight = entry.find("div", class_="weight").text
 
+        # Put it into a horse entry and add it to the race information
+        horse_entry.append(placement)
+        horse_entry.append(horse_name)
+        horse_entry.append(jockey)
+        horse_entry.append(trainer)
+        horse_entry.append(weight)
+        race_information.append(horse_entry)
+
+    return race_information
 
 if __name__ == "__main__":
     upper_bound = 1 # Change to number of races to scrape
     for id in range(upper_bound):
-        scrape_race_by_id(id)
+        info = scrape_race_by_id(id)
+        print(info)
